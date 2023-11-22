1d0d40c87075563eeebce0ea4b6caa235b045d53
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-11-22 23:27:17 +0700

    adding users seeds

---
+++ seeds/users.js
@@ -0,0 +1,45 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+const bcrypt = require('bcrypt');
+
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('users').del();
+
+  // Hash passwords
+  const hashedPasswords = await Promise.all([
+    bcrypt.hash('admin123', 10),
+    bcrypt.hash('recruiter123', 10),
+    bcrypt.hash('user123', 10),
+  ]);
+
+  // Insert hashed passwords
+  await knex('users').insert([
+    {
+      photo: 'default.jpg',
+      fullname: 'Admin',
+      telephone: '082323123',
+      email: 'admin@gmail',
+      password: hashedPasswords[0],
+      role: 'admin'
+    },
+    {
+      photo: 'default.jpg',
+      fullname: 'Recruiter',
+      telephone: '086328328',
+      email: 'recruiter@gmail',
+      password: hashedPasswords[1],
+      role: 'recruiter'
+    },
+    {
+      photo: 'default.jpg',
+      fullname: 'User',
+      telephone: '08343487834',
+      email: 'user@gmail',
+      password: hashedPasswords[2],
+      role: 'user'
+    }
+  ]);
+};
