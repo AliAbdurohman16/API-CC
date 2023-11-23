cf87d5c2b4f8900f49e79659655d6f8beb8f88c1
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-11-23 11:17:43 +0700

    updating chats migrations

--- migrations/20231123032817_create_chats_table.js
+++ migrations/20231123032817_create_chats_table.js
@@ -11,7 +11,7 @@ exports.up = function(knex) {
     table.string('attachment', 255);
     table.enum('is_read', ['0', '1']).defaultTo('0');
     table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
 
     table.foreign('sender_id', 'chats_sender_id_foreign').references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
     table.foreign('recipient_id', 'chats_recipient_id_foreign').references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
