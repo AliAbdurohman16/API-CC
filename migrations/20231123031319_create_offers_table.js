0f873112f0a125dc3d0a8d08e55b46d955ea18ac
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-11-23 11:10:13 +0700

    updating offers migration

--- migrations/20231123031319_create_offers_table.js
+++ migrations/20231123031319_create_offers_table.js
@@ -10,13 +10,13 @@ exports.up = function(knex) {
     table.integer('address_id').unsigned().notNullable();
     table.string('tariff', 255);
     table.text('experience');
-    table.enum('status', ['Pending', 'Diterima', 'Ditolak']);
+    table.enum('status', ['Pending', 'Diterima', 'Ditolak']).defaultTo('Pending');
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
 
-    table.foreign('user_id').references('id').inTable('users');
-    table.foreign('work_id').references('id').inTable('works');
-    table.foreign('address_id').references('id').inTable('addresses');
+    table.foreign('user_id', 'offers_user_id_foreign').references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
+    table.foreign('work_id', 'offers_work_id_foreign').references('id').inTable('works').onDelete('CASCADE').onUpdate('CASCADE');
+    table.foreign('address_id', 'offers_address_id_foreign').references('id').inTable('addresses').onDelete('CASCADE').onUpdate('CASCADE');
   });
 };
 
@@ -25,5 +25,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('offers'); 
 };
