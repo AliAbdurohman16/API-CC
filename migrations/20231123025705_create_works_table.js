43bcd8446d43e47340a6d617f1f9382da729b10b
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-11-23 11:04:00 +0700

    updating works migration

--- migrations/20231123025705_create_works_table.js
+++ migrations/20231123025705_create_works_table.js
@@ -18,7 +18,12 @@ exports.up = function(knex) {
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
 
-    table.foreign('category_id').references('id').inTable('categories');
+    table
+      .foreign('category_id', 'works_category_id_foreign')
+      .references('id')
+      .inTable('categories')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
   });
 };
 
