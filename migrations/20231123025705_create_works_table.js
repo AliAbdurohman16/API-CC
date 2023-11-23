71580b24ce6f79a73975b2fbae5298e4481a468c
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-11-23 10:09:49 +0700

    adding works migrations

---
+++ migrations/20231123025705_create_works_table.js
@@ -0,0 +1,31 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('works', function(table) {
+    table.increments('id').primary();
+    table.string('image', 255);
+    table.string('title', 255);
+    table.integer('category_id').unsigned().notNullable();
+    table.string('telephone', 15);
+    table.string('budget', 255);
+    table.enum('type_of_work', ['Kerja Lepas', 'Part Time', 'Full Time', 'Kontrak']);
+    table.date('start_date');
+    table.text('description');
+    table.string('latitude', 255);
+    table.string('longitude', 255);
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+
+    table.foreign('category_id').references('id').inTable('categories');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('works');
+};
