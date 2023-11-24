ab325da1ce5dda87304e934c1a424a9661cc874a
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-11-24 09:48:19 +0700

    adding controllers

---
+++ src/controllers/authController.js
@@ -0,0 +1,58 @@
+const Bcrypt = require('bcrypt');
+const Config = require('../../config/config');
+const UserModel = require('../models/User');
+const Validation = require('../utils/validation/authValidation');
+const JWT = require('@hapi/jwt');
+
+const registerUser = async (request, h) => {
+    try {
+        const validatedData = await Validation.registerSchema.validateAsync(request.payload);
+
+        // Hash password before saving to the database
+        const hashedPassword = await Bcrypt.hash(validatedData.password, 10);
+        delete validatedData.confirm_password;
+        validatedData.password = hashedPassword;
+        validatedData.role = 'user';
+
+        const userId = await UserModel.createUser(validatedData);
+
+        // Create JWT token
+        const token = JWT.token.generate({ userId, role: 'user' }, Config.jwtSecret);
+
+        return h.response({
+            code: 201,
+            message: 'success',
+            token: token,
+        }).code(201);
+    } catch (error) {
+        return h.response({ message: error.message }).code(400);
+    }
+};
+
+const loginUser = async (request, h) => {
+    try {
+        const validatedData = await Validation.loginSchema.validateAsync(request.payload);
+
+        const user = await UserModel.findUserByEmail(validatedData.email);
+        if (!user || user.role !== 'user') {
+            throw new Error('Invalid email credentials');
+        }
+        
+        const isPasswordValid = await Bcrypt.compare(validatedData.password, user.password);
+        if (!isPasswordValid) {
+            throw new Error('Invalid password credentials');
+        }
+
+        const token = JWT.token.generate({ userId: user.id, role: user.role }, Config.jwtSecret);
+
+        return h.response({
+            code: 200,
+            message: 'success',
+            token: token,
+        }).code(200);
+    } catch (error) {
+        return h.response({ message: error.message }).code(401);
+    }
+};
+
+module.exports = { registerUser, loginUser };
